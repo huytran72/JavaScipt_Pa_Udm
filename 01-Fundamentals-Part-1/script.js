23d8eb6ea18d93c193c56c2fed76cf6be367e845
Huy Tran <baohuy82.th@gmail.com>
2024-01-04 20:40:39 -0800

    Update

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,5 +1,24 @@
 let js = 'amazing';
-if (js === 'amazing') alert ('JavaScript is FUN!');
-
-40+8+23-10;
 console.log(40+8+23-10);
+
+console.log("Crown");
+console.log(25);
+
+let firstName = "Crown";
+console.log(firstName); 
+console.log(firstName); 
+console.log(firstName); 
+
+let crown_kenny = "CK";
+let $func = 27;
+
+let PI = 3.1416;
+
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'Teacher';
+
+let job1 = 'programmer';
+let job2 = 'teacher';
+
+console.log(myFirstJob);
+
