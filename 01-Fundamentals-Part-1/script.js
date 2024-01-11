48d3157e8d50d6c95fe0fc3eec911aacbfc2996b
Huy Tran <baohuy82.th@gmail.com>
2024-01-11 12:59:10 -0800

    Update

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -283,9 +283,17 @@ console.log(Boolean('Jonas'));
 console.log(Boolean({}));
 console.log(Boolean(''));
 
-const money = 0;
+const money = 10;
 if(money) {
     console.log("Don't spend it all;");
 }else {
     console.log('You should get a job!');
+}
+
+
+let height;
+if(height) {
+    console.log('YAY! Height is defined');
+}else {
+    console.log('Height is UNDEFINED');
 }
