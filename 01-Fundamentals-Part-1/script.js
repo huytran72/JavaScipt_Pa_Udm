6b4630d4d8045c958be5a2004fb155fdebbe6c75
Huy Tran <baohuy82.th@gmail.com>
2024-01-10 22:23:03 -0800

    Update

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -248,3 +248,16 @@ console.log(inputYear + 18);
 console.log(Number(inputYear) + 18);
 
 console.log(Number('Jonas'));
+console.log(typeof NaN);
+
+console.log(String(23), 23);
+
+console.log('I am ' + 23 + ' years old');
+console.log('I am ' + 23 + ' years old');
+console.log('I am ' + String(23) + ' years old');
+console.log('23' - '10' - 3);
+console.log('23' * '2');
+
+let n = '1' + 1;
+n = n - 1;
+console.log(n);
