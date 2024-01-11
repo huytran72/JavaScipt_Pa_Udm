f5d7ab0ac4efbd85e426154042027e45e66d52bd
Huy Tran <baohuy82.th@gmail.com>
2024-01-11 12:13:47 -0800

    Update

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -281,3 +281,11 @@ console.log(Boolean(0));
 console.log(Boolean(undefined));
 console.log(Boolean('Jonas'));
 console.log(Boolean({}));
+console.log(Boolean(''));
+
+const money = 0;
+if(money) {
+    console.log("Don't spend it all;");
+}else {
+    console.log('You should get a job!');
+}
