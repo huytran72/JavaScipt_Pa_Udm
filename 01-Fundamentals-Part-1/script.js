cf25750516b317b2b0a54efa5f3b4ad6e59db51e
Huy Tran <baohuy82.th@gmail.com>
2024-01-13 11:50:55 -0800

    Update

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -366,6 +366,8 @@ if (hasDriverLicense && hasGoodVision && !isTired) {
 const scoreDolphins = (96 + 108 + 89)/3;
 const scoreKoalas = (88 + 91 + 110)/3;
 
+console.log(scoreDolphins, scoreKoalas);
+
 if(scoreDolphins > scoreKoalas) {
     console.log("Dolphins win the trophy");
 } else if ( scoreDolphins < scoreKoalas) {
@@ -373,3 +375,10 @@ if(scoreDolphins > scoreKoalas) {
 } else {
     console.log("Both win the trophy");
 }
+
+
+
+// The Switch Statement
+console.log('---------------------');
+console.log('The Switch Statement');
+
