445acaa294ee4be210ff83d229a4cf648aa5b759
Huy Tran <baohuy82.th@gmail.com>
2024-01-12 23:13:04 -0800

    Update

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -342,3 +342,13 @@ const hasDriverLicense = true;  // A
 const hasGoodVision = false;     // B
 
 console.log(hasDriverLicense && hasGoodVision);
+console.log(hasDriverLicense || hasGoodVision);
+console.log(!hasDriverLicense);
+
+const shouldDrive = hasDriverLicense && hasGoodVision;
+
+if (shouldDrive) {
+    console.log('Sarah is able to drive!');
+} else {
+    console.log('Someone else should drive...');
+}
