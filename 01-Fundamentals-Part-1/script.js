3fd1d599ed918811eaa7c0d888731f77b5c6b62e
Huy Tran <baohuy82.th@gmail.com>
2024-01-15 21:49:43 -0800

    Update

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -466,3 +466,14 @@ console.log('The Conditional (Ternary) Operator');
 
 const ages = 23;
 ages >= 18 ? console.log('I like to drink wine') : console.log('I like to drink water ');
+
+const drink = ages >= 18 ? 'wine' : 'water';
+console.log(drink);
+
+let drink2;
+if(ages >= 18) {
+    drink2 = 'wine';
+} else {
+    drink2 = 'water';
+}
+console.log(drink2);
