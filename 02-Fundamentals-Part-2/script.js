d51cbe05f4bfaefdfba278633eba5e0f2913cf8f
Huy Tran <baohuy82.th@gmail.com>
2024-01-16 12:14:58 -0800

    Update

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -11,3 +11,25 @@ if(hasDriversLicense) console.log('I can drive');
 
 // const interface = 'Audio';
 // const private = 534;
+
+
+//Functions
+console.log('------------------');
+console.log('Functions');
+
+function logger() {
+    console.log('My name is Kenny');
+}
+
+//calling / running/ invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+    console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+fruitProcessor(5, 0);
