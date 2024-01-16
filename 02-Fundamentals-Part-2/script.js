99c4b3f9c68141f30af91a051999646790a21992
Huy Tran <baohuy82.th@gmail.com>
2024-01-16 11:57:38 -0800

    Update

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -1,4 +1,10 @@
 //Activating Strict Mode
-console.log('--------------------');
 console.log('Activating Strict Mode');
 
+'use strict';
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if(passTest) hasDriverLicense = true;
+if(hasDriversLicense) console.log('I can drive');
