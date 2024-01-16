f2ec173188ceddf04562967e0dddc77ff2b8bc71
Huy Tran <baohuy82.th@gmail.com>
2024-01-16 13:50:26 -0800

    Update

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -53,3 +53,18 @@ console.log(describeCountry('Japan', 99, 'Tokyo'));
 //Function Declarations VS Expressions
 console.log('----------------------------');
 console.log('Function Declarations VS Expressions');
+
+
+//Function declaration
+function calcAge1(birthYear) {
+    return 2037 - birthYear;
+}
+const age1 = calcAge1(1991);
+console.log(age1);
+
+//Function Expression
+const calcAge2 = function (birthYear) {
+    return 2037 - birthYear;
+}
+const age2 = calcAge2(1991);
+console.log(age2);
