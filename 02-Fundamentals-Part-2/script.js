fcaf4103966b534cbd6cc4934e91c06a411fe92a
Huy Tran <baohuy82.th@gmail.com>
2024-01-17 11:32:04 -0800

    Update

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -118,3 +118,19 @@ console.log(percentageOfWorld3(1441));
 console.log('-----------------------');
 console.log('Functions Calling Other Functions');
 
+function cutFruitPieces(fruit) {
+    return fruit *4;
+}
+
+function fruitProcessor(apples, oranges) {
+
+    const applePieces = cutFruitPieces(apples);
+    const orangePieces = cutFruitPieces(oranges);
+
+    console.log(apples, oranges);
+    const juice = `Juice with ${applePieces} pieces of apples and ${orangePieces} pieces of oranges.`;
+    return juice;
+}
+
+console.log(fruitProcessor(2,3));
+
