dd8954ec591a7406997705af31db723e7747bafe
Huy Tran <baohuy82.th@gmail.com>
2024-01-17 22:32:10 -0800

    Update

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -150,3 +150,24 @@ const descJapan = describePopulation('Japan', 99);
 const descEngland = describePopulation('England', 150);
 
 console.log(descAmerica, descJapan, descEngland);
+
+
+//Challenge #1
+const calcAverage = (score1, score2, score3) => (score1 + score2 + score3)/3;
+console.log(calcAverage(3,4,5));
+    
+const scoreDolphins = calcAverage(44,23,71);
+const scoreKoalas = calcAverage(65,54,49);
+console.log(scoreDolphins, scoreKoalas);
+
+function checkWinner(avgDolphins, avgKoalas) {
+    if(avgDolphins >= (avgKoalas*2)) {
+        console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`);
+    } else if (avgKoalas >= (avgDolphins*2)) {
+        console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins})`);
+    } else {
+        console.log('No team wins...');
+    }
+}
+
+checkWinner(scoreDolphins,scoreKoalas);
