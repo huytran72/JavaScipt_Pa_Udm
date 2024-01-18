33557564623880d436d10b699b2e83ae9ae683c8
Huy Tran <baohuy82.th@gmail.com>
2024-01-18 09:33:59 -0800

    Update

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -175,3 +175,10 @@ checkWinner(scoreDolphins,scoreKoalas);
 //Introduction to Arrays
 console.log('---------------------');
 console.log('Introduction to Arrays');
+
+const friend1 = 'Michael';
+const friend2 = 'Steven';
+const friend3 = 'Peter';
+
+const friends = ['Michael', 'Steven', 'Peter'];
+console.log(friends);
