c344fbb29d9c0f6f2853c3ef3f3c10f9001f59ac
Huy Tran <baohuy82.th@gmail.com>
2024-01-21 22:36:29 -0800

    Update

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -257,16 +257,13 @@ if(friends_1.includes('Steven')) {
 }
 
 //Challenge #2
-console.log('----------Challenge #2----------');
 function calcTip(bill) {
-    if(bill >=50 && bill <= 300) {
-        const tip = 15 * bill / 100;
-        console.log(tip);
-    } else {
-        tip = 20 * bill/100;
-        console.log(tip);
-    }
-    return bill;
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 }
 
-calcTip(125);
+const bills = [125, 555,44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+console.log(bills, tips);
+
+const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+console.log(bills, tips, totals);
