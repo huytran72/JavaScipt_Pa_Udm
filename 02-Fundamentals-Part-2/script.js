a103e417dbc118bd7684b427fea21866ed741425
Huy Tran <baohuy82.th@gmail.com>
2024-01-24 15:53:23 -0800

    Update

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -387,12 +387,14 @@ const myCountry1 = {
     neighbours: ['Norway', 'Sweden', 'Russia'],
 
     describe: function() {
-        return `${this.country} has ${this.population} million ${this.language}-speaking people, ${this.neighbours.length} neighbouring countries and a capital called ${this.capital}.`;
+        return `${this.country} has ${this.population} million ${this.language}-speaking people, ${this.neighbours.length} neighbouring countries and a capital called ${this.capital}.`
     },
 
     checkIsland: function() {
-        
+        this.isIsland = this.neighbours.length === 0 ? true : false;
     }
 }
 
 console.log(myCountry1.describe());
+myCountry1.checkIsland();
+console.log(myCountry1);
