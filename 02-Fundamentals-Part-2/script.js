961f9251b7704d16c2b1e5884c275dde5885a008
Huy Tran <baohuy82.th@gmail.com>
2024-01-29 17:02:50 -0800

    Update

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -571,3 +571,10 @@ while(rep <= 10) {
     console.log(`WHILE: Lifting weight repetition ${rep}`);
     rep++;
 }
+
+let dice = Math.trunc(Math.random() * 6) + 1;
+console.log(dice);
+
+while(dice !== 6) {
+    console.log(`You rolled a ${dice}`);
+}
