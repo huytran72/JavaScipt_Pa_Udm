c20fe1d7c1c9a5bcf9f7310e7b05d506f597c091
Huy Tran <baohuy82.th@gmail.com>
2024-01-29 19:02:34 -0800

    Update

--- 03-Developer-Skills/script.js
+++ 03-Developer-Skills/script.js
@@ -4,8 +4,7 @@
 //Challenge #1
 console.log('Challenge #1');
 
-const arr = [17, 21, 23];
+const data1 = [17, 21, 23];
+const data2 = [12, 5, -5, 0, 4];
 
-const printForecast = function(arr) {
-    console.log(`${arr[1]} C in 1 days. `);
-}
+console.log(`${data1[0]} C... ${data1[1]} C ... ${data1[2]} C ...`);
