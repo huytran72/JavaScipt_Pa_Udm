c451009a196eef2bf1e01e14dd265494df52343b
Huy Tran <baohuy82.th@gmail.com>
2024-01-29 18:58:37 -0800

    Update

--- 03-Developer-Skills/script.js
+++ 03-Developer-Skills/script.js
@@ -1,3 +1,11 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+//Challenge #1
+console.log('Challenge #1');
+
+const arr = [17, 21, 23];
+
+const printForecast = function(arr) {
+    console.log(`${arr[1]} C in 1 days. `);
+}
