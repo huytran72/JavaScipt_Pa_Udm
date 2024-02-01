c03e3117f7e0ac3552edbd65bbc37f4d385f0a29
Huy Tran <baohuy82.th@gmail.com>
2024-02-01 13:12:38 -0800

    Update

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -4,4 +4,7 @@ console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = 'Correct Number!✅';
 
 document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 20;
+document.querySelector('.score').textContent = 10;
+
+console.log(document.querySelector('.guess').value);
+document.querySelector('.guess').value = 23;
