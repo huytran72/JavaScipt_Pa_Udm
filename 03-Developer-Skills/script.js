cc3fb1864442881498fd81a22962bdc00f26b041
Huy Tran <baohuy82.th@gmail.com>
2024-01-31 17:09:03 -0800

    Update

--- 03-Developer-Skills/script.js
+++ 03-Developer-Skills/script.js
@@ -12,7 +12,7 @@ console.log(`${data1[0]} C... ${data1[1]} C ... ${data1[2]} C ...`);
 const printForecast = function(arr) {
     let str = '';
     for(let i = 0; i < arr.length; i++) {
-        str = str + `${arr[i]}C in ${i+1} days.`
+        str += `${arr[i]}C in ${i+1} days.`
     }
     console.log(str);
 }
