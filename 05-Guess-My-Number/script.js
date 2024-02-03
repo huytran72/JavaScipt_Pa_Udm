3bb9f57792272911549dff2dcbbe9cdd992484b6
Huy Tran <baohuy82.th@gmail.com>
2024-02-03 12:11:06 -0800

    Update

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -10,7 +10,8 @@ document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
-const score = 20;
+let score = 20;
+
 document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function() {
@@ -22,13 +23,22 @@ document.querySelector('.check').addEventListener('click', function() {
     } else if(guess === secretNumber) {
         document.querySelector('.message').textContent = 'Correct Number! ✅';
     } else if (guess > secretNumber) {
-        document.querySelector('.message').textContent = 'Too High! 📈';
-        score--;
-        document.querySelector('.score').textContent = score;
-
+        if(score > 1) {
+            document.querySelector('.message').textContent = 'Too High! 📈';
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent = 'You lost the game!';
+            document.querySelector('.score').textContent = 0;
+        }
     } else if (guess < secretNumber) {
-        document.querySelector('.message').textContent = 'Too Low! 📉';
-        score--;
-        document.querySelector('.score').textContent = score;
+        if(score > 1) {
+            document.querySelector('.message').textContent = 'Too Low! 📉';
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent = 'You lost the game!';
+            document.querySelector('.score').textContent = 0;
+        }
     }
 });
