391e9ecbbc0712230a9519dfe2ae86c062dd0f06
Huy Tran <baohuy82.th@gmail.com>
2024-02-03 12:18:51 -0800

    Update

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -20,8 +20,14 @@ document.querySelector('.check').addEventListener('click', function() {
 
     if(!guess) {
         document.querySelector('.message').textContent = 'No number! ⛔️';
+
     } else if(guess === secretNumber) {
         document.querySelector('.message').textContent = 'Correct Number! ✅';
+
+        document.querySelector('body').style.backgroundColor = '#60b347';
+
+        document.querySelector('.number').style.width = '30rem';
+
     } else if (guess > secretNumber) {
         if(score > 1) {
             document.querySelector('.message').textContent = 'Too High! 📈';
@@ -31,6 +37,7 @@ document.querySelector('.check').addEventListener('click', function() {
             document.querySelector('.message').textContent = 'You lost the game!';
             document.querySelector('.score').textContent = 0;
         }
+
     } else if (guess < secretNumber) {
         if(score > 1) {
             document.querySelector('.message').textContent = 'Too Low! 📉';
