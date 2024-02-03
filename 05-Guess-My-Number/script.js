fe242dbd71cd39e430eeb7b5bfcafc2aa4d41bd9
Huy Tran <baohuy82.th@gmail.com>
2024-02-02 23:50:05 -0800

    Update

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -9,8 +9,8 @@ document.querySelector('.score').textContent = 10;
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
-const number = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = number;
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function() {
     const guess = Number(document.querySelector('.guess').value);
@@ -18,5 +18,7 @@ document.querySelector('.check').addEventListener('click', function() {
 
     if(!guess) {
         document.querySelector('.message').textContent = 'No number! ⛔️';
+    } else if(guess === secretNumber) {
+        document.querySelector('.message').textContent = 'Correct Number! ✅';
     }
 });
