df8f50ce81381c39206caa0843d96d2c004423c8
Huy Tran <baohuy82.th@gmail.com>
2024-02-06 14:03:58 -0800

    Update

--- 06-Modal/script.js
+++ 06-Modal/script.js
@@ -22,4 +22,8 @@ btnOpenModal[i].addEventListener('click', openModal);
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 
+document.addEventListener('keydown', function() {
+    console.log('A key was pressed'); 
+})
+
 
