e6633bb2d5cd62e8efc81f326c0883884d94531d
Huy Tran <baohuy82.th@gmail.com>
2024-02-06 12:04:29 -0800

    Update

--- 06-Modal/script.js
+++ 06-Modal/script.js
@@ -7,20 +7,20 @@ const btnOpenModal = document.querySelectorAll('.show-modal');
 console.log(btnOpenModal);
 
 for(let i = 0; i < btnOpenModal.length; i++) 
-    btnOpenModal[i].addEventListener('click', function() {
+
+btnOpenModal[i].addEventListener('click', function() {
         console.log('Button clicked');
         modal.classList.remove('hidden');
         overlay.classList.remove('hidden');
     });
 
-    btnCloseModal.addEventListener('click', function() {
-        modal.classList.add('hidden');
-        overlay.classList.add('hidden');
-    })
 
-    overlay.addEventListener('click', function() {
-        modal.classList.add('hidden');
-        overlay.classList.add('hidden');
-    })
+const closeModal = function() {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
 
 
