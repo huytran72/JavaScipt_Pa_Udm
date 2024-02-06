fafa931727e323a20f2e767d2fd23871ff8cadc1
Huy Tran <baohuy82.th@gmail.com>
2024-02-06 11:32:02 -0800

    Update

--- 06-Modal/script.js
+++ 06-Modal/script.js
@@ -7,5 +7,10 @@ const btnOpenModal = document.querySelectorAll('.show-modal');
 console.log(btnOpenModal);
 
 for(let i = 0; i < btnOpenModal.length; i++) {
-    console.log(btnOpenModal[i].textContent);
+    btnOpenModal[i].addEventListener('click', function() {
+        console.log('Button clicked');
+        modal.classList.remove('hidden');
+        overlay.classList.remove('hidden');
+    });
 }
+
