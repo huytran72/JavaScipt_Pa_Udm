85fd92a0d6e2fa80a586e355f546450e9c9e0954
Huy Tran <baohuy82.th@gmail.com>
2024-02-06 00:05:11 -0800

    Update

--- 06-Modal/script.js
+++ 06-Modal/script.js
@@ -3,5 +3,9 @@
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
-const btnOpenModal = document.querySelector('.open-modal');
+const btnOpenModal = document.querySelectorAll('.show-modal');
 console.log(btnOpenModal);
+
+for(let i = 0; i < btnOpenModal.length; i++) {
+    console.log(btnOpenModal[i].textContent);
+}
