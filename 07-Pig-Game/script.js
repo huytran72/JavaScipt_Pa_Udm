ea193ae23bcde072de1880e883a16c25dbd43942
Huy Tran <baohuy82.th@gmail.com>
2024-02-07 13:21:51 -0800

    Update

--- 07-Pig-Game/script.js
+++ 07-Pig-Game/script.js
@@ -3,6 +3,8 @@
 // Selecting elements
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
@@ -29,7 +31,7 @@ btnRoll.addEventListener('click', function() {
     if(dice !== 1) {
         //Add dice to current score
         currentScore += dice;
-
+        current0El.textContent = currentScore; //Change later
     } else {
         // Switch to next player
     }
