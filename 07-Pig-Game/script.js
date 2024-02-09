ea0bd867cf995c96542b1d895073d3b199504b28
Huy Tran <baohuy82.th@gmail.com>
2024-02-09 09:43:38 -0800

    Update

--- 07-Pig-Game/script.js
+++ 07-Pig-Game/script.js
@@ -64,9 +64,11 @@ btnHold.addEventListener('click', function() {
     // 2. Check if player's score is >= 100
     // Finish the game
     if(scores[activePlayer] >= 100) {
-        document.querySelector(`player--${activePlayer}`).
+        document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
+        document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
+    } else {
+        // Switch to the next player
+        switchPlayer();
     }
     
-    // Switch to the next player
-    switchPlayer();
 });
