8c245b49e7c279ecacfd071c826ce450e03856ae
Huy Tran <baohuy82.th@gmail.com>
2024-02-10 10:44:52 -0800

    Update

--- 07-Pig-Game/script.js
+++ 07-Pig-Game/script.js
@@ -15,14 +15,28 @@ const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
 // Starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+
+
+
+const init = function() {
+    const scores = [0, 0];
+    let currentScore = 0;
+    let activePlayer = 0;
+    let playing = true;
+
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    current0El.textContent = 0;
+    current1El.textContent = 0;
+
+    diceEl.classList.add('hidden');
+    player0El.classList.remove('player--winner');
+    player1El.classList.remove('player--winner');
+    player1El.classList.add('player--active');
+    player1El.classList.remove('player--active');
+}
+init();
 
 // Function Switch to next player
 const switchPlayer = function() {
@@ -77,4 +91,8 @@ btnHold.addEventListener('click', function() {
         switchPlayer();
     }
 }
+});
+
+btnNew.addEventListener('click', function() {
+    
 });
