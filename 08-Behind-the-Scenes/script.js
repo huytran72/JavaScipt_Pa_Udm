797ec44c4c01a47bf878220a62abe052ef2c98ea
Huy Tran <baohuy82.th@gmail.com>
2024-02-11 23:48:28 -0800

    Update

--- 08-Behind-the-Scenes/script.js
+++ 08-Behind-the-Scenes/script.js
@@ -6,6 +6,12 @@ function calcAge(birthYear) {
     function printAge() {
         const output = `${firstName}, You are ${age}, born in ${birthYear}`;
         console.log(output);
+        
+        if(birthYear >= 1981 && birthYear <= 1996) {
+            const str = `Oh, and you're a millenial, ${firstName}`;
+            console.log(str);
+        }
+
     }
     printAge();
 
