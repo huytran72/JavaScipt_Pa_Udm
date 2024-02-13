19c2e408fa3c5ae90ab88af4ff509e8fbab05806
Huy Tran <baohuy82.th@gmail.com>
2024-02-13 14:21:52 -0800

    Update

--- 08-Behind-the-Scenes/script.js
+++ 08-Behind-the-Scenes/script.js
@@ -4,12 +4,19 @@ function calcAge(birthYear) {
     const age = 2037 - birthYear;
 
     function printAge() {
-        var millenial = true;
-        const firstName = 'Steven';
+        
         let output = `${firstName}, You are ${age}, born in ${birthYear}`;
         console.log(output);
         
         if(birthYear >= 1981 && birthYear <= 1996) {
+            var millenial = true;
+            
+            //Creating NEW variable with same name as outer scope's variable
+            const firstName = 'Steven';
+
+            //Reassigning outer scope's variable
+            output = 'NEW OUTPUT!'
+
             const str = `Oh, and you're a millenial, ${firstName}`;
             console.log(str);
 
@@ -17,7 +24,6 @@ function calcAge(birthYear) {
                 return a + b;
             }
 
-            output = 'NEW OUTPUT!'
         }
         // console.log(str);
         console.log(millenial);
