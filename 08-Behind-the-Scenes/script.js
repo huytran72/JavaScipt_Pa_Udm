bf0f24b84e57c6117f02e230cfa64097ef852075
Huy Tran <baohuy82.th@gmail.com>
2024-02-13 23:53:06 -0800

    Update

--- 08-Behind-the-Scenes/script.js
+++ 08-Behind-the-Scenes/script.js
@@ -45,3 +45,16 @@ calcAge(1991);
 var me = 'Percy';
 let job = 'student';
 const year = 1991;
+
+//Functions
+console.log(addDecl(2,3));
+
+function addDecl(a,b) {
+    return a +b;
+}
+
+const addExpr = function(a,b) {
+    return a+b;
+}
+
+const addArrow = (a,b) => a+b;
