e32aff85fa7005012716d166c88348bc41366bb1
Huy Tran <baohuy82.th@gmail.com>
2024-02-18 23:29:33 -0800

    Update

--- 08-Behind-the-Scenes/script.js
+++ 08-Behind-the-Scenes/script.js
@@ -205,8 +205,15 @@ const jessica2 = {
     firstName: 'Jessica',
     lastName: 'Williams',
     age: 27,
+    family: ['Alice', 'Bob'],
 };
 const jessicaCopy = Object.assign({}, jessica2);
 jessicaCopy.lastName = 'Davis';
+console.log('Before marriage:', jessica2);
+console.log('After marriage:', jessicaCopy);
+
+jessicaCopy.family.push('Mary');
+jessicaCopy.family.push('John');
+
 console.log('Before marriage:', jessica2);
 console.log('After marriage:', jessicaCopy);
