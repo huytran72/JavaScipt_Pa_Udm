dc8ea31f3139ce3433a72bc838c7157584b1b710
Huy Tran <baohuy82.th@gmail.com>
2024-02-20 12:04:36 -0800

    Update

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -11,6 +11,10 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  order: function(starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  }
 };
 
 const arr = [2,3,4];
@@ -36,3 +40,7 @@ console.log(main, secondary);
 [main, secondary] = [secondary, main];
 console.log(main, secondary);
 
+
+//Receive 2 return values from a function
+const [start, mainCourse] = restaurant.order(2,0);
+console.log(start, mainCourse);
