37111f20bd0f98e1b4b3d7e31b98e78f7d6be56e
Huy Tran <baohuy82.th@gmail.com>
2024-02-22 11:50:10 -0800

    Update

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -100,7 +100,9 @@ console.log(sat, weekdays);
 
 // 2. Functions
 const add = function(...numbers) {
-  console.log(numbers);
+  let sum = 0;
+  for(let i = 0; i<numbers.length; i++) sum += numbers[i];
+  console.log(sum);
 }
 add(2,3);
 add(5,3,7,2);
