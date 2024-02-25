51f36c9882d9954a9caa221cd803d9ee0ecaa900
Huy Tran <baohuy82.th@gmail.com>
2024-02-24 23:45:09 -0800

    Update

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -175,3 +175,10 @@ console.log(1,2,7,8,9);
 
 const newMenu = [...restaurant.mainMenu, 'Gnocci'];
 console.log(newMenu);
+
+//Copy array
+const mainMenuCopy = [...restaurant.mainMenu];
+
+
+// Join 2 arrays
+const menu = [...restaurant.starterMenu, ]
