d16132e80c4341c0a1cf6614d9dd0f56bc3d9cc5
Huy Tran <baohuy82.th@gmail.com>
2024-02-27 23:48:57 -0800

    Update

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -257,5 +257,8 @@ const rest2 = {
 rest1.numGuests = rest1.numGuests || 10;
 rest2.numGuests = rest2.numGuests || 10;
 
+rest1.numGuests ||= 10;
+rest2.numGuests ||= 10;
+
 console.log(rest1);
 console.log(rest2);
