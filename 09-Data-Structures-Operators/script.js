c29b6ff62fbfc3671c6f0687f581d70325af23da
Huy Tran <baohuy82.th@gmail.com>
2024-03-06 11:59:35 -0800

    Update

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -327,3 +327,5 @@ console.log(allPlayers);
 const players1Final = [...players1, 'Thiago', 'Coutinho', 'Periscic'];
 
 //5.
+const {odds: {team1, x: draw, team2}} = game;
+console.log(team1, draw. team2);
