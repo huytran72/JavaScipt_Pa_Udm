f9762302c74e40f3aa037fac3fcbe7e6152cf912
Huy Tran <baohuy82.th@gmail.com>
2024-03-13 11:47:35 -0700

    Update

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -485,3 +485,10 @@ console.log(rest.set(2, 'Lisbon, Portugal'));
 
 rest
   .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open')
+  .set(false, 'We are closed');
+
+  console.log(rest.get('name'));
+  console.log(rest.get(true));
