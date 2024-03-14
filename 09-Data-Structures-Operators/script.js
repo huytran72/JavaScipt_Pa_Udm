28f57b20e990cb0d9a9c998c69b808a16930d11f
Huy Tran <baohuy82.th@gmail.com>
2024-03-14 00:41:28 -0700

    Update

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -490,8 +490,11 @@ rest
   .set(true, 'We are open')
   .set(false, 'We are closed');
 
-  console.log(rest.get('name'));
-  console.log(rest.get(true));
-  console.log(rest.get(1));
+console.log(rest.get('name'));
+console.log(rest.get(true));
+console.log(rest.get(1));
 
-  const time = 21;
+const time = 21;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+console.log(rest.has('categories'));
