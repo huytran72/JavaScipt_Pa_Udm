d412623b57c86c32e875253113cd268865f17b67
Huy Tran <baohuy82.th@gmail.com>
2024-03-15 09:46:44 -0700

    Update

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -537,3 +537,5 @@ console.log(question.get('question'));
 for(const [key, value] of question) {
   if(typeof key === 'number') console.log(`Answer ${key}: ${value}`);
 }
+const answer1 = Number(prompt('Your answer'));
+console.log(answer1);
