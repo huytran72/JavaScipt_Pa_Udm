ca1617a6f8c23950fc5c343a492d43b4073644dc
Huy Tran <baohuy82.th@gmail.com>
2024-03-18 13:04:54 -0700

    Update

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -602,3 +602,10 @@ const airline = 'TAP Air Portugal';
 const plane = 'A320';
 
 console.log(plane[0]);
+console.log(plane[1]);
+console.log(plane[2]);
+console.log(plane[3]);
+console.log(plane[4]);
+
+console.log('B737'[0]);
+console.log(airline.length);
