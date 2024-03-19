3c02068e58679e83ed6777c5933a00199a4564b5
Huy Tran <baohuy82.th@gmail.com>
2024-03-19 13:07:36 -0700

    Update

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -667,3 +667,8 @@ console.log(trimmedEmail === lowerEmail);
 
 //Replacing
 const priceGB = '288,97£';
+const priceUS = priceGB.replace('£', '$').replace(',', '.');
+console.log(priceUS);
+
+const announcement = 'All passengers come to boarding door 23. Boarding door 23!';
+console.log(announcement.replace('door', 'gate'));
