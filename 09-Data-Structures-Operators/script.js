578f13d1741a9f902d87a1f626957c7506b4bf9c
Huy Tran <baohuy82.th@gmail.com>
2024-03-23 00:36:01 -0700

    Update

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -786,20 +786,13 @@ document.querySelector('button').addEventListener('click', function() {
 console.log('-----------------');
 console.log('String Methods Practice');
 
-const flights1 = 'LH234';
-const flights2 = 'LH235';
-const flights3 = 'LH236';
-const flights4 = 'LH237';
+const flights = 
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
-const airline1 = 'TAP Air Portugal';
-const plane2 = 'A320';
-
-console.log(flights1.slice(2));
-console.log(flights1.slice(1));
-console.log(flights1.slice(1, 3));
-console.log(flights1.slice(1, -1));
-console.log(flights1.slice(1, -2));
-
+//Delayed Departure from FAO to TXL (11h25)
+//Arrival from BRU to FAO (11h45)
+//Delayed Arrival at HEL from FAO (12h05)
+//Departure from FAO to LIS (12h30)
 
 
 
