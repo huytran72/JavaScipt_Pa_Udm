8a661aaf5cc0f58470599f37771f5d4f628207ea
Huy Tran <baohuy82.th@gmail.com>
2024-03-25 21:23:37 -0700

    Update

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const createBooking = function (flightNum, numPassengers = 1, price = 199) {
+  // ES5
+  // numPassengers = numPassengers || 1;
+  // price = price || 199;
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+}
+
+
