1a7a05ce68639de8f0d0b85300950fd683fcaef7
Huy Tran <baohuy82.th@gmail.com>
2024-03-26 12:44:44 -0700

    Update

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -71,6 +71,16 @@ const upperFirstWord = function (str) {
     return [first.toUpperCase(), ...others].join(' ');
 }
 
+// Higher-order function
+const transformer = function (str, fn) {
+    console.log(`Original string: ${str}`);
+    console.log(`Transformed string: ${fn(str)}`);
+    console.log(`Transformed by: ${fn.name}`);
+}
+
+transformer('JavaScript is the best!', upperFirstWord);
+transformer('JavaScript is the best!', oneWord);
+
 
 
 
