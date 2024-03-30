2f50e3420881286681769096789d7595ac619f8a
Huy Tran <baohuy82.th@gmail.com>
2024-03-30 12:01:49 -0700

    Update

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -217,3 +217,32 @@ console.log(addVAT2(100));
 // Coding Challenge #1
 console.log('--------------------');
 console.log('Coding Challenge #1');
+
+const poll = {
+    question: 'What is your favorite programming language?',
+    options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+    answers: new Array(4).fill(0),
+
+    registerNewAnswer() {
+        const answer = Number(prompt(`${this.question}\n${this.options.join('\n')}\n(Write option number)`));
+
+        if (typeof answer === 'number' && answer < this.answers.length) {
+            this.answers[answer]++;
+        }
+
+        this.displayResults();
+        this.displayResults('string');
+    },
+
+    displayResults(type = 'array') {
+        if (type === 'array') {
+            console.log(this.answers);
+        } else if (type === 'string') {
+            console.log(`Poll results are ${this.answers.join(', ')}`);
+        }
+    },
+};
+
+document.querySelector('.poll').addEventListener('click', poll.registerNewAnswer.bind(poll));
+
+// poll.displayResults.call({ answers: [5, 2, 3] }, 'string');
