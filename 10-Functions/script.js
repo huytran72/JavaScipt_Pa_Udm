3502f9285611958fe2668fecf1568712edfbdf35
Huy Tran <baohuy82.th@gmail.com>
2024-03-30 11:59:54 -0700

    Update

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -198,3 +198,9 @@ console.log('Partial Application');
 
 const addTax = (rate, value) => value + value * rate;
 console.log(addTax(0.1, 200));
+
+const addVAT = addTax.bind(null, 0.23);
+// addVAT = value => value + value * 0.23;
+
+console.log(addVAT(100));
+console.log(addVAT(23));
