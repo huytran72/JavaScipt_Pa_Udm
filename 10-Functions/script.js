a819e933dfa26000f48cd31eb72fe02b60033e59
Huy Tran <baohuy82.th@gmail.com>
2024-04-04 11:38:15 -0700

    Update

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -327,4 +327,12 @@ console.dir(f);
 // Example 2
 const boardPassengers = function (n, wait) {
     const perGroup = n / 3;
+    
+        setTimeout(function () {
+            console.log(`We are now boarding all ${n} passengers`);
+            console.log(`There are 3 groups, each with ${perGroup} passengers`);
+        }, wait * 1000);
+    
+        console.log(`Will start boarding in ${wait} seconds`);
+    }
 
