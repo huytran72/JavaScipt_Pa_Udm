51d5a609869de7bfa224c345cd6abcd72a0f734d
Huy Tran <baohuy82.th@gmail.com>
2024-04-10 12:49:29 -0700

    Update script.js

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -417,3 +417,5 @@ lufthansa.buyPlane = function () {
     this.planes++;
     console.log(this.planes);
 };
+
+document.querySelector('.buy').addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
