1f2d41b3a5e508c4c033a282ab862e97832bd112
Huy Tran <baohuy82.th@gmail.com>
2024-04-13 22:41:56 -0700

    Update script.js

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -443,4 +443,6 @@ const poll = {
     question: 'What is your favorite programming language?',
     options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
     answers: new Array(4).fill(0),
+
+    registerNewAnswer() {
    
