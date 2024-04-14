890ef57e14432dadb45d44abd4807524e9178c0c
Huy Tran <baohuy82.th@gmail.com>
2024-04-14 01:14:09 -0700

    Update script.js

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -457,4 +457,6 @@ const poll = {
 };
 
 document.querySelector('.poll').addEventListener('click', poll.registerNewAnswer.bind(poll));
+
+poll.displayResults.call({ answers: [5, 2, 3] }, 'string');
    
