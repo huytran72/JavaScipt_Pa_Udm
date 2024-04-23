1750f50b939c9e77309eabe66f5aeb42056a3554
Huy Tran <baohuy82.th@gmail.com>
2024-04-23 12:42:16 -0700

    Update script.js

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -81,3 +81,4 @@ let arr = ['a', 'b', 'c', 'd', 'e'];
 console.log(arr.slice(2)); // ['c', 'd', 'e']
 console.log(arr.slice(2, 4)); // ['c', 'd']
 console.log(arr.slice(-2)); // ['d', 'e']
+console.log(arr.slice(-1)); // ['e']
