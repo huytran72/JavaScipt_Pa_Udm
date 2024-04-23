f971f3194729e38c0830a51fc92dacbca3e4adf8
Huy Tran <baohuy82.th@gmail.com>
2024-04-23 12:39:02 -0700

    Update script.js

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -74,3 +74,8 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+
+let arr = ['a', 'b', 'c', 'd', 'e'];
+
+// SLICE
+console.log(arr.slice(2)); // ['c', 'd', 'e']
