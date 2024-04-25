5c3f66a8e0b72ecbff49abbcdb501882143e86a3
Huy Tran <baohuy82.th@gmail.com>
2024-04-24 23:43:12 -0700

    Update script.js

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -107,3 +107,10 @@ console.log(letters.join(' - ')); // a - b - c - d - e - f - g - h - i - j
 
 // forEach
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+for (const [i, movement] of movements.entries()) {
+  if (movement > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${movement}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
+  }
+}
