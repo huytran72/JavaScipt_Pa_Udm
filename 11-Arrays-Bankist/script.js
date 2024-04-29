e3bb92c6d05168a82a43bd9a7a438bcb37b31616
Huy Tran <baohuy82.th@gmail.com>
2024-04-29 12:17:08 -0700

    Update script.js

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -188,3 +188,5 @@ const checkDogs = function (dogsJulia, dogsKate) {
     console.log(`Dog number ${i + 1} is ${message}`);
   });
 checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
+}
