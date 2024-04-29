f5b916e4718253f3fe68c0262ca6b2a52f0752e5
Huy Tran <baohuy82.th@gmail.com>
2024-04-29 12:17:50 -0700

    Update script.js

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -193,3 +193,4 @@ checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
 
 // Data Transformation: Map, Filter, Reduce
 console.log('---------------------------------');
+console.log('--- Data Transformation: Map, Filter, Reduce ---');
