b2e197499046e58746463b69cf95771382ed06ac
Huy Tran <baohuy82.th@gmail.com>
2024-05-18 00:22:19 -0700

    Update script.js

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -423,6 +423,7 @@ console.log(movements);
 // More ways
 console.log([1, 2, 3, 4, 5, 6, 7, 8, 9]);
 console.log([1, 2, 3, 4, 5, 6, 7, 8, 9].sort((a, b) => a - b));
+console.log([1, 2, 3, 4, 5, 6, 7, 8, 9].sort((a, b) => b - a));
 
 
 
