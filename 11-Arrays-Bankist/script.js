199f2072d93708f05aed356b53a3893617030d79
Huy Tran <baohuy82.th@gmail.com>
2024-05-22 23:26:22 -0700

    Update script.js

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -488,6 +488,8 @@ console.log(convertTitleCase('this is a nice title'));
 console.log(convertTitleCase('this is a LONG title but not too long'));
 console.log(convertTitleCase('and here is another title with an EXAMPLE'));
 
+// Coding Challenge #4
+
 
 
 
