1dc4d15c222ffe63fb013fd93c0001637c44206f
Huy Tran <baohuy82.th@gmail.com>
2024-05-28 08:31:37 -0700

    Update script.js

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -274,6 +274,9 @@ let balance2 = 0;
 for (const mov of movements) balance2 += mov;
 console.log(balance2);
 
+
+
+
 // Maximum value
 console.log('---------------------------------');
 console.log('--- Maximum value ---');
