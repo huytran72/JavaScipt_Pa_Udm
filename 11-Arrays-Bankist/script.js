99b123976000eb80df89c387db9006918b452bb1
Huy Tran <baohuy82.th@gmail.com>
2024-05-29 13:08:53 -0700

    Update script.js

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -301,14 +301,13 @@ console.log('--- Coding Challenge #2 ---');
 
 const calcAverageHumanAge = function (ages) {
   const humanAges = ages.map(age => age <= 2 ? 2 * age : 16 + age * 4);
-  console.log(humanAges);
-
   const adults = humanAges.filter(age => age >= 18);
+  console.log(humanAges);
   console.log(adults);
 
   const average = adults.reduce((acc, age) => acc + age, 0) / adults.length;
   console.log(average);
-}
+};
 
 calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
 calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
