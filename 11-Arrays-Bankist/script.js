ddd7f46d77cd688c1703e3ca31aecb15220729e1
Huy Tran <baohuy82.th@gmail.com>
2024-05-29 13:05:51 -0700

    Update script.js

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -289,8 +289,7 @@ console.log('--- Maximum value ---');
 const max = movements.reduce(function (acc, mov) {
   if (acc > mov) return acc;
   else return mov;
-}
-, movements[0]);
+}, movements[0]);
 
 console.log(max);
 
