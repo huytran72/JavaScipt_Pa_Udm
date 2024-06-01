18d468b0a8877c802022a266847fbbbea587f927
Huy Tran <baohuy82.th@gmail.com>
2024-05-31 23:14:16 -0700

    Update script.js

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -77,6 +77,7 @@ calcDisplaySummary = function (movements) {
   .filter(mov => mov > 0)
   .map(deposit => deposit * 1.2 / 100)
   .filter((int, i, arr) => {
+    console.log(arr);
   .reduce((acc, int) => acc + int, 0);
   labelSumInterest.textContent = `${interest} EUR`;
 }
