8a90775a59a5f5c96e16bac8b6312a9c0406df84
Huy Tran <baohuy82.th@gmail.com>
2024-06-10 16:20:35 -0700

    Update script.js

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -498,6 +498,9 @@ console.log('--- Some and Every ---');
 console.log(movements);
 console.log(movements.includes(-130));
 
+//Condition
+console.log(movements.some(mov => mov === -130));
+
 // Some: Condition
 const anyDeposits = movements.some(mov => mov > 1500);
 console.log(anyDeposits);
