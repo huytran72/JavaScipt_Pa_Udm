f1dfd82339d0c3f5f51ff534cc3bf620b4360594
Huy Tran <baohuy82.th@gmail.com>
2024-06-14 23:47:35 -0700

    Update script.js

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -509,6 +509,8 @@ btnLoan.addEventListener('click', function (e) {
 btnLoan.addEventListener('click', function (e) {
   e.preventDefault();
 
+});
+
 btnClose.addEventListener('click', function (e) {
   e.preventDefault();
   inputCloseUsername.value = inputClosePin.value = '';
