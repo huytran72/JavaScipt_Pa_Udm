2af01b1de40e1a5f6cd519dbd400a3ca949e8d4b
Huy Tran <baohuy82.th@gmail.com>
2024-06-21 23:19:55 -0700

    Update script.js

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -754,8 +754,7 @@ const numDeposits1000 = accounts
   .flatMap(acc => acc.movements)
   // .reduce((count, mov) => (mov >= 1000 ? count + 1 : count), 0);
   .reduce((count, mov) => (mov >= 1000 ? count + 1 : count), 0);
-  .filter(mov => mov >= 1000)
-  .length;
+  
 console.log(numDeposits1000);
 
 
