dc85b453c8232fc876da7b50c599e977be3b3f94
Huy Tran <baohuy82.th@gmail.com>
2024-06-21 23:18:50 -0700

    Update script.js

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -749,6 +749,7 @@ console.log(bankDepositSum);
 // 2. Count how many deposits in the bank with at least 1000  in it
 // const numDeposits1000 = accounts
 //   .flatMap(acc => acc.movements)
+
 const numDeposits1000 = accounts
   .flatMap(acc => acc.movements)
   .filter(mov => mov >= 1000)
