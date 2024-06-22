0376a4fad16d37860a102863f68ccd55d7a7aa5f
Huy Tran <baohuy82.th@gmail.com>
2024-06-21 23:16:08 -0700

    Update script.js

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -724,8 +724,10 @@ console.log(w);
 
 labelBalance.addEventListener('click', function () {
   const movementsUI = Array.from(document.querySelectorAll('.movements__value'), el => Number(el.textContent.replace('€', '')));
-  
+
   console.log(movementsUI);
+
+  const movementsUI2 = [...document.querySelectorAll('.movements__value')];
 }
 );
 
