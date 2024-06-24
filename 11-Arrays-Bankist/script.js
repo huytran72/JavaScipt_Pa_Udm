bd25eabca14dc20f5fa277e282482cde99238891
Huy Tran <baohuy82.th@gmail.com>
2024-06-23 22:45:33 -0700

    Update script.js

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -804,6 +804,7 @@ const dogs = [
   { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
   { weight: 32, curFood: 340, owners: ['Michael'] },
 ];
+// 1.
 
 document.body.append(document.createElement('textarea'));
 document.body.append(document.createElement('button'));
