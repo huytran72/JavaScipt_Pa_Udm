ea85732e6e58faf8c84d6f7c1520465a76fb5df7
Huy Tran <baohuy82.th@gmail.com>
2024-06-26 23:19:56 -0700

    Update script.js

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -851,6 +851,7 @@ console.log(dogs.some(dog => dog.curFood === dog.recommendedFood));
 
 
 //6.
+const checkEatingOkay = dog => dog.curFood > dog.recommendedFood * 0.9 && dog.curFood < dog.recommendedFood * 1.1;
 
 
 // Working with Arrays: A Recap
