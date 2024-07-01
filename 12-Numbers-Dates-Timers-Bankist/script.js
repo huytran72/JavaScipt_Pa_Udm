3c4ac5208a99079d09d31bfb2f38a0431a2632fb
Huy Tran <baohuy82.th@gmail.com>
2024-06-30 23:39:55 -0700

    Update script.js

--- 12-Numbers-Dates-Timers-Bankist/script.js
+++ 12-Numbers-Dates-Timers-Bankist/script.js
@@ -274,3 +274,5 @@ btnSort.addEventListener('click', function (e) {
 // console.log(Number.isNaN('20'));
 // console.log(Number.isNaN(+'20X'));
 // console.log(Number.isNaN(23 / 0));
+
+// console.log(Number.isFinite(20));
