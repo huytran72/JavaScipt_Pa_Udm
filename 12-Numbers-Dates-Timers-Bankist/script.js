32893eb6832abfc3469eee4f777cb5b65d719049
Huy Tran <baohuy82.th@gmail.com>
2024-07-10 21:07:23 -0700

    Update script.js

--- 12-Numbers-Dates-Timers-Bankist/script.js
+++ 12-Numbers-Dates-Timers-Bankist/script.js
@@ -326,6 +326,8 @@ console.log('Math and Rounding');
 
 console.log('------------------------------------');
 console.log('Remainder Operator');
+
+
 // Remainder operator
 // console.log(5 % 2); // 1
 // console.log(5 / 2); // 2.5
