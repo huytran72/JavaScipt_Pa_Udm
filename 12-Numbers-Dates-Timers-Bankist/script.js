f9c5a337d5e18bbd068e7eba20c47f4b30fb73b4
Huy Tran <baohuy82.th@gmail.com>
2024-07-11 23:06:29 -0700

    Update script.js

--- 12-Numbers-Dates-Timers-Bankist/script.js
+++ 12-Numbers-Dates-Timers-Bankist/script.js
@@ -368,6 +368,10 @@ console.log('Numeric Separators');
 // Numeric Separators
 //287,460,000,000
 const diameter = 287_460_000_000;
+console.log(diameter);
+
+
+
 // console.log(23 === 23.0);  
 
 // Base 10 - 0 to 9. 1/10 = 0.1, 3/10 = 3.  
