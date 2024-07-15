64f7d56a34f9e4578e5ab2be0086aad7c88901c3
Huy Tran <baohuy82.th@gmail.com>
2024-07-14 23:29:51 -0700

    Update script.js

--- 12-Numbers-Dates-Timers-Bankist/script.js
+++ 12-Numbers-Dates-Timers-Bankist/script.js
@@ -390,6 +390,7 @@ console.log('Working with BigInt');
 // console.log(2 ** 53 - 1); // 9007199254740991
 // console.log(Number.MAX_SAFE_INTEGER); // 9007199254740991
 // console.log(2 ** 53 + 1); // 9007199254740992
+// console.log(2 ** 53 + 2); // 9007199254740994
 
 
 
