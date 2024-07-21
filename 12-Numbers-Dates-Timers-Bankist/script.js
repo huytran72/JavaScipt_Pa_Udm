96fc19b104d33577abbf2043bf16716b7db77c38
Huy Tran <baohuy82.th@gmail.com>
2024-07-20 23:56:50 -0700

    Update script.js

--- 12-Numbers-Dates-Timers-Bankist/script.js
+++ 12-Numbers-Dates-Timers-Bankist/script.js
@@ -470,6 +470,8 @@ const calcDaysPassed = (date1, date2) => Math.abs(date2 - date1) / (1000 * 60 * 
 const days1 = calcDaysPassed(new Date(2037, 3, 14), new Date(2037, 3, 4));
 console.log(days1);
 
+console.log('------------------------------------');
+
 
 
 
