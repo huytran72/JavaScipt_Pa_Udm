29eff4e63a9767554bfedea6f49b2103f5342042
Huy Tran <baohuy82.th@gmail.com>
2024-07-22 21:14:43 -0700

    Update script.js

--- 12-Numbers-Dates-Timers-Bankist/script.js
+++ 12-Numbers-Dates-Timers-Bankist/script.js
@@ -502,6 +502,7 @@ const options = {
 console.log('US: ', new Intl.NumberFormat('en-US', options).format(num));
 console.log('Germany: ', new Intl.NumberFormat('de-DE', options).format(num));
 console.log('Syria: ', new Intl.NumberFormat('ar-SY', options).format(num));
+console.log('Nigeria: ', new Intl.NumberFormat('en-NG', options).format(num));
 
 
 
