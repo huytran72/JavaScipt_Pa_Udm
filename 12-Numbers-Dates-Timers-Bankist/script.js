70c73b42627eae1d7e3e1bea6eff26ba88149a0a
Huy Tran <baohuy82.th@gmail.com>
2024-07-23 23:57:52 -0700

    Update script.js

--- 12-Numbers-Dates-Timers-Bankist/script.js
+++ 12-Numbers-Dates-Timers-Bankist/script.js
@@ -516,6 +516,7 @@ console.log('setTimeout and setInterval');
 const ingredients = ['olives', 'spinach'];
 
 const pizzaTimer = setTimeout((ing1, ing2) => console.log(`Here is your pizza with ${ing1} and ${ing2}`), 3000, ...ingredients);
+console.log('Waiting...');
 
 
 
