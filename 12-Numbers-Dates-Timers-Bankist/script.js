f7eaf5c7490df91d3a57b4ac48ab20037c8dbc50
Huy Tran <baohuy82.th@gmail.com>
2024-07-23 23:57:56 -0700

    Update script.js

--- 12-Numbers-Dates-Timers-Bankist/script.js
+++ 12-Numbers-Dates-Timers-Bankist/script.js
@@ -517,6 +517,7 @@ const ingredients = ['olives', 'spinach'];
 
 const pizzaTimer = setTimeout((ing1, ing2) => console.log(`Here is your pizza with ${ing1} and ${ing2}`), 3000, ...ingredients);
 console.log('Waiting...');
+if (ingredients.includes('spinach')) clearTimeout(pizzaTimer);
 
 
 
