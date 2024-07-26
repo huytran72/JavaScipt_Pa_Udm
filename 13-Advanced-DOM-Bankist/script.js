42e9c3295f03ebbe0535c5db48ab52b01806e9bb
Huy Tran <baohuy82.th@gmail.com>
2024-07-25 23:26:04 -0700

    Update script.js

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -37,3 +37,5 @@ document.addEventListener('keydown', function (e) {
 console.log(document.documentElement);
 console.log(document.head);
 console.log(document.body);
+
+document.querySelector('.header');
