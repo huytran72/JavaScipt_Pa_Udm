edb99680c3909c35612fd347663a37a4fea51062
Huy Tran <baohuy82.th@gmail.com>
2024-07-27 23:13:46 -0700

    Update script.js

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -58,3 +58,4 @@ message.classList.add('cookie-message');
 message.innerHTML = 'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
 const header = document.querySelector('.header');
 // header.prepend(message);
+header.append(message);
