10c7ed06489021dd8ea2018b8d829cb795bf4ee2
Huy Tran <baohuy82.th@gmail.com>
2024-08-01 23:38:54 -0700

    Update script.js

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -118,3 +118,5 @@ const h1 = document.querySelector('h1');
 const alertH1 = function (e) {
   alert('addEventListener: Great! You are reading the heading :D');
 }
+
+h1.addEventListener('mouseenter', alertH1);
