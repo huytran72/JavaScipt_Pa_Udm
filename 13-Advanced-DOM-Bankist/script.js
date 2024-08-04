49336fac7b758cf19e88e7a749e677e4fda734de
Huy Tran <baohuy82.th@gmail.com>
2024-08-03 23:54:55 -0700

    Update script.js

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -157,3 +157,4 @@ document.querySelector('.nav').addEventListener('click', function (e) {
 const h1 = document.querySelector('h1');
 
 // Going downwards: child
+console.log(h1.querySelectorAll('.highlight'));
