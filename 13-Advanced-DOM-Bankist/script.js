a3717c18db5309c0717310ce93ad01180540f327
Huy Tran <baohuy82.th@gmail.com>
2024-08-07 15:07:20 -0700

    Update script.js

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -219,3 +219,9 @@ const stickyNav = function (entries) {
   if (!entry.isIntersecting) nav.classList.add('sticky');
   else nav.classList.remove('sticky');
 }
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
