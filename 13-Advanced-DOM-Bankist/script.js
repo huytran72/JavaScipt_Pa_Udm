1785492e3c91d614f66bb0910187e8ead8500643
Huy Tran <baohuy82.th@gmail.com>
2024-08-08 23:12:50 -0700

    Update script.js

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -239,3 +239,7 @@ const revealSection = function (entries, observer) {
   observer.unobserve(entry.target);
 }
 
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
