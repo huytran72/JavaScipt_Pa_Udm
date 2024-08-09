5e162524bad6dd49fb86f131702e33adf0985434
Huy Tran <baohuy82.th@gmail.com>
2024-08-08 23:13:23 -0700

    Update script.js

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -254,3 +254,15 @@ const imgTargets = document.querySelectorAll('img[data-src]');
 console.log(imgTargets);
 
 const loadImg = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img');
+  });
+
+  observer.unobserve(entry.target);
+}
