dd604bea925127be84bfd8097593a623fc42f7e1
Huy Tran <baohuy82.th@gmail.com>
2024-08-11 00:04:57 -0700

    Update script.js

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -71,6 +71,13 @@ document.querySelector('.btn--close-cookie').addEventListener('click', function 
 });
 
 //Page navigation
+document.querySelectorAll('.nav__link').forEach(function (el) {
+  el.addEventListener('click', function (e) {
+    e.preventDefault();
+    const id = this.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  });
+});
 
 // Styles
 message.style.backgroundColor = '#37383d';
