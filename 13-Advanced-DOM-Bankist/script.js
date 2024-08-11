9d15c5867797e1f37dd7dbf4aa2d416ed76c086a
Huy Tran <baohuy82.th@gmail.com>
2024-08-11 00:02:01 -0700

    Update script.js

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -111,17 +111,17 @@ logo.classList.toggle('c');
 logo.classList.contains('c');
 
 // Don't use
-logo.className = 'jonas';
+// logo.className = 'jonas';
 
-const h1 = document.querySelector('h1');
+// const h1 = document.querySelector('h1');
 
-const alertH1 = function (e) {
-  alert('addEventListener: Great! You are reading the heading :D');
-}
+// const alertH1 = function (e) {
+//   alert('addEventListener: Great! You are reading the heading :D');
+// }
 
-h1.addEventListener('mouseenter', alertH1);
+// h1.addEventListener('mouseenter', alertH1);
 
-setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
+// setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
 
 // h1.onmouseenter = function (e) {
 //   alert('onmouseenter: Great! You are reading the heading :D');
@@ -286,4 +286,4 @@ const slider = function () {
 
 
 
-  
+
