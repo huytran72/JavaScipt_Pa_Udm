d2ae792597a1f3924b2be88c0ef5a18562aee55c
Huy Tran <baohuy82.th@gmail.com>
2024-08-13 23:57:29 -0700

    Update script.js

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -219,6 +219,7 @@ h1.closest('h1').style.background = 'var(--gradient-secondary)';
 h1.closest('h1').style.background = 'var(--gradient-primary)';
 
 // h1.closest('.header').style.background = 'var(--gradient-secondary)';
+// h1.closest('.header').style.background = 'var(--gradient-primary)';
 
 // Going sideways: siblings
 console.log(h1.previousElementSibling);
