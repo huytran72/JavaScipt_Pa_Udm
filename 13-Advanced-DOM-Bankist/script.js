82818f81d2d2268f6e7cc3e7704d408e4e69e01e
Huy Tran <baohuy82.th@gmail.com>
2024-08-13 23:58:10 -0700

    Update script.js

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -228,6 +228,8 @@ h1.closest('h1').style.background = 'var(--gradient-primary)';
 console.log(h1.previousElementSibling);
 console.log(h1.nextElementSibling);
 
+console.log(h1.previousSibling);
+
 console.log(h1.parentElement.children);
 [...h1.parentElement.children].forEach(function (el) {
   if (el !== h1) el.style.transform = 'scale(0.5)';
