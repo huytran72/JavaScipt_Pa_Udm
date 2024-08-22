f4767e8d891932fb4849a3811eaa939ae648ba33
Huy Tran <baohuy82.th@gmail.com>
2024-08-22 01:12:29 -0700

    Update script.js

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -515,6 +515,15 @@ document.addEventListener('keydown', function (e) {
 }
 );
 
+dotContainer.addEventListener('click', function (e) {
+  if (e.target.classList.contains('dots__dot')) {
+    const { slide } = e.target.dataset;
+    goToSlide(slide);
+    activateDot(slide);
+  }
+}
+);
+
 
 
 
