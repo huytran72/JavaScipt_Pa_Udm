20fdccc1706f443f156c45b1c1e843625509f8da
Huy Tran <baohuy82.th@gmail.com>
2024-08-24 23:42:51 -0700

    Update script.js

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -1,4 +1,13 @@
 'use strict';
 
 const Person = function (firstName, birthYear) {
+    // Instance properties
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+    
+    // Never do this
+    // this.calcAge = function () {
+    //   console.log(2037 - this.birthYear);
+    // };
+    }
 
