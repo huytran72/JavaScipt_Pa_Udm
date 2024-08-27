c328a2f65d3741577e51054f4a58af62ee33198a
Huy Tran <baohuy82.th@gmail.com>
2024-08-26 20:17:41 -0700

    Update script.js

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -50,3 +50,4 @@ matilda.calcAge();
 console.log(jonas.__proto__);
 console.log(jonas.__proto__ === Person.prototype);
 console.log(Person.prototype.isPrototypeOf(jonas));
+console.log(Person.prototype.isPrototypeOf(matilda));
