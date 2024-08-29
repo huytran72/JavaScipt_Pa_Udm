0afe96643b2b43a713c0a88e81f442010e8f626b
Huy Tran <baohuy82.th@gmail.com>
2024-08-29 00:38:17 -0700

    Update script.js

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -95,6 +95,11 @@ Car.prototype.accelerate = function() {
     console.log(`${this.make} is going at ${this.speed} km/h`);
 };
 
+Car.prototype.accelerate = function() {
+    this.speed -= 5;
+    console.log(`${this.make} is going at ${this.speed} km/h`);
+};
+
 const bmw = new Car('BMW', 120);
 const mercedes = new Car('Mercedes', 95);
 
