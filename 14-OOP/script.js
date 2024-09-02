cdb22c9fcf886d6ca349f8ff4d81619443b3f370
Huy Tran <baohuy82.th@gmail.com>
2024-09-02 11:24:59 -0700

    Update script.js

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -228,3 +228,4 @@ sarah.init('Sarah', 1979);
 1. Re-create challenge 1, but this time using an ES6 class;
 2. Add a getter called 'speedUS' which returns the current speed in mi/h (divide by 1.6);
 3. Add a setter called 'speedUS' which sets the current speed in mi/h (but converts it to km/h before storing the value, by multiplying the input by 1.6);
+4. Create a new car and experiment with the accelerate and brake methods, and with the getter and setter.
