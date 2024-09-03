fcb33af8ae3ade14a839df17f9edd41619fca90c
Huy Tran <baohuy82.th@gmail.com>
2024-09-03 00:04:28 -0700

    Update script.js

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -277,3 +277,7 @@ const Person = function (firstName, birthYear) {
     this.firstName = firstName;
     this.birthYear = birthYear;
 };
+
+Person.prototype.calcAge = function () {
+    console.log(2037 - this.birthYear);
+};
