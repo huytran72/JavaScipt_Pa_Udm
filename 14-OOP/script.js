6a45b4a0c0161f3c9818895e148d64c99cfbbe1f
Huy Tran <baohuy82.th@gmail.com>
2024-09-04 13:15:57 -0700

    Update script.js

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -317,5 +317,5 @@ console.dir(Student.prototype.constructor);
 3. Implement a 'brake' method that will decrease the car's speed by 5, and log the new speed to the console;
 4. Create 2 'Car' objects and experiment with calling 'accelerate' and 'brake' multiple times on each of them.
 
-DATA CAR 1: 'BMW' going at 120 km/h
+DATA CAR 1: 'BMW' going at 120 km/h, and CAR 2: 'Mercedes' going at 95 km/h.
 */
