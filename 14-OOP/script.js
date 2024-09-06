9fe387a3168b0710ea52dcdbb4740093745ff95b
Huy Tran <baohuy82.th@gmail.com>
2024-09-06 11:31:05 -0700

    Update script.js

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -371,6 +371,32 @@ tesla.accelerate();
 
 // Inheritance Between "Classes": ES6 Classes
 class PersonCl {
+    constructor(fullName, birthYear) {
+        this.fullName = fullName;
+        this.birthYear = birthYear;
+    }
+
+    calcAge() {
+        console.log(2037 - this.birthYear);
+    }
+
+    greet() {
+        console.log(`Hey ${this.fullName}`);
+    }
+
+    get age() {
+        return 2037 - this.birthYear;
+    }
+
+    set fullName(name) {
+        if (name.includes(' ')) this._fullName = name;
+        else alert(`${name} is not a full name!`);
+    }
+
+    get fullName() {
+        return this._fullName;
+    }
+}
 
 // Static Methods
 static hey() {
