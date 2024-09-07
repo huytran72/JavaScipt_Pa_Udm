7a193f08c3897e8b340a678c116675dbafe18308
Huy Tran <baohuy82.th@gmail.com>
2024-09-07 13:22:01 -0700

    Update script.js

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -464,3 +464,11 @@ jay.calcAge();
 
 class Account {
     constructor(owner, currency, pin) {
+        this.owner = owner;
+        this.currency = currency;
+        this._pin = pin;
+        this.movements = [];
+        this.locale = navigator.language;
+
+        console.log(`Thanks for opening an account, ${owner}`);
+    }
