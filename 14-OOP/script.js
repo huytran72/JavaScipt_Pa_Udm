b2b96ac052141285f781f1c85de5883ce8abb526
Huy Tran <baohuy82.th@gmail.com>
2024-09-10 07:29:02 -0700

    Update script.js

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -551,5 +551,21 @@ console.log(acc1.getMovements());
 5. Test the 'EVCl' class and the 'accelerate' method.
 
 DATA CAR 1: 'Rivian' going at 120 km/h, with a charge of 23%
-
 */
+
+class CarCl {
+    constructor(make, speed) {
+        this.make = make;
+        this.speed = speed;
+    }
+
+    accelerate() {
+        this.speed += 10;
+        console.log(`${this.make} is going at ${this.speed} km/h`);
+    }
+
+    brake() {
+        this.speed -= 5;
+        console.log(`${this.make} is going at ${this.speed} km/h`);
+    }
+}
