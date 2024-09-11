102b00b85cf11c70899301b5ac19a9ef6b216838
Huy Tran <baohuy82.th@gmail.com>
2024-09-11 12:06:41 -0700

    Update script.js

--- 15-Mapty/script.js
+++ 15-Mapty/script.js
@@ -11,26 +11,26 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-navigator.geolocation.getCurrentPosition(
-    function (position) {
-        const { latitude } = position.coords;
-        const { longitude } = position.coords;
-        console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
-    },
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+        function (position) {
+            const { latitude } = position.coords;
+            const { longitude } = position.coords;
+            console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
 
-    var map = L.map('map').setView([51.505, -0.09], 13);
+            var map = L.map('map').setView([latitude, longitude], 13);
 
-    L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-    }).addTo(map);
+            L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+                maxZoom: 19,
+            }).addTo(map);
 
-    L.marker([51.5, -0.09])
-        .addTo(map)
-        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-        .openPopup();
-    },
-
-    function () {
-        alert('Could not get your position');
-    }
-);
+            L.marker([latitude, longitude])
+                .addTo(map)
+                .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+                .openPopup();
+        },
+        function () {
+            alert('Could not get your position');
+        }
+    );
+}
