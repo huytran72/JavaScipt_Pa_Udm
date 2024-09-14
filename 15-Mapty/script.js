7375fa26b9ba1bfb50e2b9f73bb1b0151647b6d2
Huy Tran <baohuy82.th@gmail.com>
2024-09-13 23:16:04 -0700

    Update script.js

--- 15-Mapty/script.js
+++ 15-Mapty/script.js
@@ -34,6 +34,9 @@ class App {
             // console.log(map);
 
             L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">
+                    OpenStreetMap</a> contributors',
+            }).addTo(map);
 
         form.addEventListener('submit', this._newWorkout.bind(this));
 
