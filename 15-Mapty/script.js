f942c7f58a4c7c179b917859227f24e6b19ac573
Huy Tran <baohuy82.th@gmail.com>
2024-09-15 11:32:52 -0700

    Update script.js

--- 15-Mapty/script.js
+++ 15-Mapty/script.js
@@ -37,6 +37,19 @@ class Running extends Workout {
 }
 
 class Cycling extends Workout {
+    type = 'cycling';
+    constructor(coords, distance, duration, elevationGain) {
+        super(coords, distance, duration);
+        this.elevationGain = elevationGain;
+        this.calcSpeed();
+    }
+
+    calcSpeed() {
+        // km/h
+        this.speed = this.distance / (this.duration / 60);
+        return this.speed;
+    }
+}
 
 let map, mapEvent;
 
