009bd6209457b68db310aadd5a5def65f31da037
Huy Tran <baohuy82.th@gmail.com>
2024-09-19 23:51:08 -0700

    Update script.js

--- 15-Mapty/script.js
+++ 15-Mapty/script.js
@@ -29,6 +29,8 @@ class Workout {
         const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
         this.description = `${this.type[0].toUpperCase()}${this.type.slice(1)} on ${months[this.date.getMonth()]} ${this.date.getDate()}`;
     }
+
+    click() {
 }
 
 class Running extends Workout {
