ee7288912bb816b547a94e08408049ef921695e0
Huy Tran <baohuy82.th@gmail.com>
2024-09-26 00:51:54 -0700

    Update script.js

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -59,6 +59,7 @@ const getCountryAndNeighbour = function (country) {
         // AJAX call country 2
         const request2 = new XMLHttpRequest();
         request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbour}`);
+        request2.send();
     });
 };
 
