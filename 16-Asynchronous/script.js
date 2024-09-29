67f5639611c7b6defec4494194245b08796027f5
Huy Tran <baohuy82.th@gmail.com>
2024-09-29 14:46:42 -0700

    Update script.js

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -141,6 +141,7 @@ const getCountryData = function (country) {
             return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
         })
         .then(response => response.json())
+        .then(data => renderCountry(data, 'neighbour'))
 };
 
 getCountryData('portugal');
