768bb10d3f59f0bf240f0a91797a44538fc83a75
Huy Tran <baohuy82.th@gmail.com>
2024-09-30 22:11:56 -0700

    Update script.js

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -130,6 +130,7 @@ console.log(request1);
 
 const renderError = function (msg) {
     countriesContainer.insertAdjacentText('beforeend', msg);
+    countriesContainer.style.opacity = 1;
 
 
 const getCountryData = function (country) {
