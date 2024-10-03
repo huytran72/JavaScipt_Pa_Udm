34c88d71c7096856df04b92424db59819ed22454
Huy Tran <baohuy82.th@gmail.com>
2024-10-02 23:31:31 -0700

    Update script.js

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -208,6 +208,7 @@ Here are your tasks:
 PART 1
 1. Create a function 'whereAmI' which takes as input a latitude value ('lat') and a longitude value ('lng') (these are GPS coordinates).
 2. Do 'reverse geocoding' of the provided coordinates. Reverse geocoding is the process of converting geographic coordinates into a human-readable address.
+3. Use this API to do reverse geocoding: https://geocode.xyz/api. The AJAX call will be done to a URL with this format: 'https://geocode.xyz/52.508,13.381?json=1'.
 
 
 
