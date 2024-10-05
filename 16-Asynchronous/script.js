e854d7de0008eaf3e13140b20813122c9aafeaaa
Huy Tran <baohuy82.th@gmail.com>
2024-10-05 12:32:59 -0700

    Update script.js

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -268,7 +268,7 @@ Promise.resolve('Resolved promise 1').then(res => console.log(res));
 console.log('Test end');
 
 Promise.resolve('Resolved promise 2').then(res => {
-    for (let i = 0; i < 1000000000; i++) {}
+    for (let i = 0; i < 1000000000000; i++) {}
     console.log(res);
 }
 );
