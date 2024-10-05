0bdefca6772702427d4a06b58b1a44e1c2de7f9a
Huy Tran <baohuy82.th@gmail.com>
2024-10-05 12:31:20 -0700

    Update script.js

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -265,4 +265,5 @@ whereAmI(-33.933, 18.474);
 console.log('Test start');
 setTimeout(() => console.log('0 sec timer'), 0);
 Promise.resolve('Resolved promise 1').then(res => console.log(res));
+console.log('Test end');
 
