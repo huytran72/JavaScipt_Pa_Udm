652330a4a3ece9a72299d4578c23bc5e80e7f88a
Huy Tran <baohuy82.th@gmail.com>
2024-10-05 12:31:52 -0700

    Update script.js

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -267,3 +267,5 @@ setTimeout(() => console.log('0 sec timer'), 0);
 Promise.resolve('Resolved promise 1').then(res => console.log(res));
 console.log('Test end');
 
+Promise.resolve('Resolved promise 2').then(res => {
+
