8920db5b99395c9896a9aa1d32d6e513872922ca
Huy Tran <baohuy82.th@gmail.com>
2024-10-09 23:59:31 -0700

    Update script.js

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -391,5 +391,6 @@ If this part is too tricky for you, just watch the first part of the solution.
 PART 2
 2. Comsume the promise using .then and also add an error handler.
 3. After the image has loaded, pause execution for 2 seconds using the 'wait' function we created earlier.
+4. After the 2 seconds have passed, hide the current image (set display CSS property to 'none'), and load a second image (Hint: Use the image element returned by the 'createImage' promise to hide the current image. You will need a global variable for that 😉).
 
 */
