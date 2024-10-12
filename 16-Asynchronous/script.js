cf1ec3c47878dccb54ebcf15f664db9908e90ac9
Huy Tran <baohuy82.th@gmail.com>
2024-10-12 14:10:14 -0700

    Update script.js

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -457,6 +457,7 @@ const whereAmI2 = async function(country) {
     console.log(res);
 
     fetch(`https://restcountries.com/v3.1/name/${country}`)
+    const data = await res.json();
     console.log(res);
 };
 whereAmI2('portugal');
