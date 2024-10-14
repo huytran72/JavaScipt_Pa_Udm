1eb4c30ed908cd6a5650d786468ef315ffd1b4f7
Huy Tran <baohuy82.th@gmail.com>
2024-10-13 22:54:49 -0700

    Update script.js

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -463,6 +463,7 @@ const whereAmI2 = async function(country) {
     const {} = await getPosition();
     const {latitude: lat, longitude: lng} = pos.coords;
 
+        const resGeo = await fetch(`https://geocode.xyz/${lat},${lng}?json=1`);
     // const res = await fetch(`https://restcountries.com/v3.1/name/${country}`)
     // console.log(res);
 
