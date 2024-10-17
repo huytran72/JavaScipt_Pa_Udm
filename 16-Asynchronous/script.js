d5fb238bf252f6db36919a079917657816f32dfe
Huy Tran <baohuy82.th@gmail.com>
2024-10-16 22:44:47 -0700

    Update script.js

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -538,6 +538,8 @@ const get3Countries = async function(c1, c2, c3) {
         const [data2] = await getJSON(`https://restcountries.com/v3.1/name/${c2}`);
         const [data3] = await getJSON(`https://restcountries.com/v3.1/name/${c3}`);
 
+        Promise.all([
+
         console.log(data1.capital, data2.capital, data3.capital);
 
     } catch(err) {
