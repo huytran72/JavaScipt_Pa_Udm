b1609cef7d12b17968eca709d0693878f8e402b6
Huy Tran <baohuy82.th@gmail.com>
2024-10-16 22:45:37 -0700

    Update script.js

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -534,9 +534,9 @@ console.log('3: Finished getting location');
 
 const get3Countries = async function(c1, c2, c3) {
     try {
-        const [data1] = await getJSON(`https://restcountries.com/v3.1/name/${c1}`);
-        const [data2] = await getJSON(`https://restcountries.com/v3.1/name/${c2}`);
-        const [data3] = await getJSON(`https://restcountries.com/v3.1/name/${c3}`);
+        // const [data1] = await getJSON(`https://restcountries.com/v3.1/name/${c1}`);
+        // const [data2] = await getJSON(`https://restcountries.com/v3.1/name/${c2}`);
+        // const [data3] = await getJSON(`https://restcountries.com/v3.1/name/${c3}`);
 
         Promise.all([ 
             getJSON(`https://restcountries.com/v3.1/name/${c1}`),
