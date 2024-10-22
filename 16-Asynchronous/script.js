33db8f147d49c750b85172607765ddf9b693f666
Huy Tran <baohuy82.th@gmail.com>
2024-10-21 23:48:35 -0700

    Update script.js

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -634,4 +634,5 @@ PART 2
 2. Use .map to loop over the array, to load all the images with the 'createImage' function (call the resulting array 'imgs')
 3. Check out the 'imgs' array in the console! Is it like you expected?
 4. Use a promise to log the image from the array as soon as it is loaded.
+5. Part 2 is more difficult, so don't worry if you don't get it. Just watch the solution.
 */
