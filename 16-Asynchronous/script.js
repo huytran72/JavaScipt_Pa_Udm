fc6e54fca297331cd653bcfb18e56dfa9a180904
Huy Tran <baohuy82.th@gmail.com>
2024-10-26 14:01:27 -0700

    Update script.js

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -709,6 +709,7 @@ const loadNPause1 = async function() {
         // Load image 2
         let img2 = await createImage('img/img-2.jpg');
         console.log('Image 2 loaded');
+        await wait2(2);
 
 
     } catch(err) {
