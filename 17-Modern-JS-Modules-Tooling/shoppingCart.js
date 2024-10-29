91a8e2adcfd7f96dc18baaa3c2c5bf99a2380f9c
Huy Tran <baohuy82.th@gmail.com>
2024-10-29 12:25:34 -0700

    Update shoppingCart.js

--- 17-Modern-JS-Modules-Tooling/shoppingCart.js
+++ 17-Modern-JS-Modules-Tooling/shoppingCart.js
@@ -4,7 +4,7 @@ console.log('shoppingCart.js');
 const shippingCost = 10;
 const cart = [];
 
-const addToCart = function (product, quantity) {
+export const addToCart = function (product, quantity) {
     cart.push({ product, quantity });
     console.log(`${quantity} ${product} added to cart`);
 };       
