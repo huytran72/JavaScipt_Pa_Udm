730e03ee48221626170453e9186ea88ae239a742
Huy Tran <baohuy82.th@gmail.com>
2024-10-30 20:45:34 -0700

    Update script.js

--- 17-Modern-JS-Modules-Tooling/script.js
+++ 17-Modern-JS-Modules-Tooling/script.js
@@ -9,3 +9,4 @@ console.log(totalPrice, totalQuantity);
 
 // import * as ShoppingCart from './shoppingCart.js';
 // ShoppingCart.addToCart('bread', 5);
+// console.log(ShoppingCart.totalPrice, ShoppingCart.totalQuantity);
