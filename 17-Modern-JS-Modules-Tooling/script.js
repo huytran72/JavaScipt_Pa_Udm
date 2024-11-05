28fae023b59e4a3dab652c50d8cfb626094843de
Huy Tran <baohuy82.th@gmail.com>
2024-11-04 21:12:58 -0800

    Update script.js

--- 17-Modern-JS-Modules-Tooling/script.js
+++ 17-Modern-JS-Modules-Tooling/script.js
@@ -107,5 +107,6 @@ import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
 const state = {
     cart: [
         {product: 'bread', quantity: 5},
+        {product: 'pizza', quantity: 5},
     ]
 }
